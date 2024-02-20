6556c0e4275dd839e5b23214988f4d99c557f0ac
andro <andro@DESKTOP-52JASCQ>
2024-02-20 21:09:49 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(Console.ReadLine());
+
+            int temp = a % 2;
+
+
+            Console.WriteLine($"temp --- {temp}");
+        }
+
+    }
+}
